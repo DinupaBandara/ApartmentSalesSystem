ca09fcd34ec23c692eacbb49ecf3233c00f328ed
Dinupa Thathsarani Bandara <dinupathathsarani@gmail.com>
2026-08-13 09:19:44 +0530

    Replace old project with apartment sales system

---
+++ src/main/java/com/apartmentSalesManagemnetSystem/ApartmentSalesManagemnetSystemApplication.java
@@ -0,0 +1,13 @@
+package com.apartmentSalesManagemnetSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentSalesManagemnetSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApartmentSalesManagemnetSystemApplication.class, args);
+    }
+
+}
